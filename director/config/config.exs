60e94b9c6aee5d60f36e31d4079a4b2fe1e53b83
jonas.deboeck@student.ucll.be <jonas.deboeck@student.ucll.be>
2020-12-03 16:35:10 +0000

    Director setup

---
+++ director/config/config.exs
@@ -0,0 +1,17 @@
+import Config
+
+config :director,
+  ecto_repos: [Director.Repo]
+
+config :director, Director.Repo,
+  database: "assignment_crypto",
+  username: "jonas",
+  password: "t",
+  hostname: "localhost"
+
+config :database_interaction, repo: Director.Repo
+
+config :director,
+  pairs_to_clone: ["BTC_ETH", "USDT_BTC", "USDC_BTC"],
+  from: 1_590_969_600,
+  until: 1_591_500_000
--- director/mix.exs
+++ director/mix.exs
@@ -24,6 +24,10 @@ defmodule Director.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:assignment_messages, git: "https://github.com/distributed-applications-2021/assignment-messages", branch: "main"},
+      {:database_interaction,
+       git: "https://github.com/distributed-applications-2021/assignment-database-interaction", branch: "main"},
+       {:myxql, "~> 0.4.3"}
     ]
   end
 end
